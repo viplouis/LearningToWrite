3bf6e8e2e520f0f5a4af72af4e644bb53ceb54a7
Apr <viplouis@outlook.com>
2018-11-09 17:43:18 +0800

    :100:

---
+++ 2018-11/Java工具类/获取指定日期的前后天数的日期(字符串日期参数).java
@@ -0,0 +1,86 @@
+package com.yunke.amazon.advertisingSyn.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class a {
+	/**
+	 * 获得指定日期的前一天
+	 * 
+	 * @param specifiedDay
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getSpecifiedDayBefore(String specifiedDay) {
+		// SimpleDateFormat simpleDateFormat = new
+		// SimpleDateFormat("yyyy-MM-dd");
+		Calendar c = Calendar.getInstance();
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("yy-MM-dd").parse(specifiedDay);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		c.setTime(date);
+		int day = c.get(Calendar.DATE);
+		c.set(Calendar.DATE, day - 1);
+
+		String dayBefore = new SimpleDateFormat("yyyy-MM-dd").format(c.getTime());
+		return dayBefore;
+	}
+
+	/**
+	 * 获得指定日期的后一天
+	 * 
+	 * @param specifiedDay
+	 * @return
+	 */
+	public static String getSpecifiedDayAfter(String specifiedDay) {
+		Calendar c = Calendar.getInstance();
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("yy-MM-dd").parse(specifiedDay);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		c.setTime(date);
+		int day = c.get(Calendar.DATE);
+		c.set(Calendar.DATE, day + 1);
+
+		String dayAfter = new SimpleDateFormat("yyyy-MM-dd").format(c.getTime());
+		return dayAfter;
+	}
+	
+	
+	
+	public static String demo(String specifiedDay,int i) {
+		
+		Calendar c = Calendar.getInstance();
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("yy-MM-dd").parse(specifiedDay);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		c.setTime(date);
+		int day = c.get(Calendar.DATE);
+		c.set(Calendar.DATE, day - i);
+
+		String dayBefore = new SimpleDateFormat("yyyy-MM-dd").format(c.getTime())+","+new SimpleDateFormat("yyyyMMdd").format(c.getTime());
+		return dayBefore;
+	}
+	
+	
+	public static void main(String[] args) {
+//		 System.out.println(getSpecifiedDayBefore("2018-11-3"));
+		List<String> dateStr = new ArrayList<String>();
+		for(int i=0;i<7;i++){
+			dateStr.add(DateUtils.getListOfDaysBefore("2018-10-25", 7-i));
+		}
+		System.out.println(dateStr.toString());
+	}
+}
